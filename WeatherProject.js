fd65eb8a450ecfbaf734d225725bb86018e3d0a0
Fanchen Bao <jonathonbao@gmail.com>
2020-01-06 18:13:33 -0500

    Add husky for pre-commit and pre-push hook. Add input validation for zipcode

--- WeatherProject.js
+++ WeatherProject.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import {StyleSheet, Text, View, TextInput} from 'react-native';
+import validate from 'validate.js';
 
 export default class WeatherProject extends Component {
   constructor(props) {
@@ -7,20 +8,56 @@ export default class WeatherProject extends Component {
     this.state = {zip: ''};
   }
 
-  _onChangeText(text) {
-    console.log(text)
+  _validateZip() {
+    let res = validate(this.state, constraints);
+    console.log(res);
   }
 
   render() {
     return (
-      <View>
+      <View style={styles.container}>
         <TextInput
-          onChangeText={text => this._onChangeText(text)}
-          placeholder={"Please input a zip code..."}
-          keyboardType={"number-pad"}
+          style={styles.input}
+          onChangeText={text => this.setState({zip: text})}
+          onSubmitEditing={() => this._validateZip()}
+          placeholder={'Input a zip code...'}
+          keyboardType={'number-pad'}
         />
-        <Text>You have input {this.state.zip}</Text>
+        <Text style={styles.welcome}>{`You have input zip code: ${
+          this.state.zip
+        }`}</Text>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {fontSize: 20, textAlign: 'center', margin: 10},
+  input: {
+    fontSize: 20,
+    borderWidth: 2,
+    padding: 2,
+    height: 40,
+    width: 200,
+    textAlign: 'center',
+  },
+});
+
+const constraints = {
+  zip: {
+    presence: {
+      allowEmpty: false,
+      message: 'required.',
+    },
+    format: {
+      pattern: /\d{5}(-\d{4})?/, // regex pattern must be enclosed by forward slashes
+      message: 'new error message',
+    },
+  },
+};
